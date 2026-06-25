2c77d1bdc41fde154b0653b3639093bace05aaca
SeanByr <seanbyrne123@outlook.ie>
2026-06-25 14:54:20 +0100

    added basic scaffolding for SecurityConfig.java, filter chain to be added after basic auth endpoints are made

---
+++ backend/src/main/java/com/roottherapy/backend/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.roottherapy.backend.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //todo add SecurityFilterChain after auth endpoints are made
+}
