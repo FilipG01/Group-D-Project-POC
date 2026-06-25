2df7f9741ce3c5dd77b8fd5e0d9f9a29435111c3
SeanByr <seanbyrne123@outlook.ie>
2026-06-25 17:10:00 +0100

    added security filter chain

--- backend/src/main/java/com/roottherapy/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/roottherapy/backend/security/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.roottherapy.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -14,5 +16,14 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    //todo add SecurityFilterChain after auth endpoints are made
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/register/client", "/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
