ebe5d6c9a171b5a15ac83faee09ff797ac56f886
SeanByr <seanbyrne123@outlook.ie>
2026-06-26 10:11:42 +0100

    added spring security user details integration

---
+++ backend/src/main/java/com/roottherapy/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.roottherapy.backend.security;
+
+
+import com.roottherapy.backend.users.User;
+import com.roottherapy.backend.users.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailIgnoreCase(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User was not found"));
+        return new CustomUserDetails(user);
+    }
+}
