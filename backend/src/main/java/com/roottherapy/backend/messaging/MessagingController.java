9d86fec53b78beed837d5974716cf8beb0c9c1b5
SeanByr <seanbyrne123@outlook.ie>
2026-07-01 14:47:37 +0100

    added endpoints for sending and listing messages

--- backend/src/main/java/com/roottherapy/backend/messaging/MessagingController.java
+++ backend/src/main/java/com/roottherapy/backend/messaging/MessagingController.java
@@ -1,6 +1,8 @@
 package com.roottherapy.backend.messaging;
 
 import com.roottherapy.backend.messaging.dto.ConversationResponse;
+import com.roottherapy.backend.messaging.dto.MessageResponse;
+import com.roottherapy.backend.messaging.dto.SendMessageRequest;
 import com.roottherapy.backend.messaging.dto.StartConversationRequest;
 import com.roottherapy.backend.security.CustomUserDetails;
 import jakarta.validation.Valid;
@@ -8,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/message/")
@@ -28,5 +31,18 @@ public class MessagingController {
     public List<ConversationResponse> listMyConversations(Authentication auth){
         CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
         return messagingService.listMyConversations(userDetails.getUser());
+    }
+
+    @GetMapping("/conversations/{conversationId/messages")
+    public List<MessageResponse> listMessage(Authentication auth, @PathVariable UUID conversationId){
+        CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
+        return messagingService.listMessages(userDetails.getUser(), conversationId);
+    }
+
+    @PostMapping("/conversations/{conversationId}/messages")
+    public MessageResponse sendMessage(Authentication auth, @PathVariable UUID conversationId,
+                                       @Valid @RequestBody SendMessageRequest req){
+        CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
+        return messagingService.sendMessage(userDetails.getUser(), conversationId, req);
     }
 }
