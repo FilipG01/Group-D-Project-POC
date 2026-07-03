495680f9724d0aa7c5cb13ca3edefa8933be23ff
SeanByr <seanbyrne123@outlook.ie>
2026-07-03 10:32:59 +0100

    added protected route component and added AuthProvider to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthContext.jsx";
+
 
 import "./styles/global.css";
 
@@ -9,7 +11,9 @@ import App from "./App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthProvider>
+                <App />
+            </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
