f3eef34c40c8b71c67fd06026b429bfff0d06742
SeanByr <seanbyrne123@outlook.ie>
2026-07-03 10:38:05 +0100

    added mantine component lib

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext.jsx";
-
+import { MantineProvider } from "@mantine/core";
 
 import "./styles/global.css";
+import "@mantine/core/styles.css";
 
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
+            <MantineProvider>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </MantineProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
