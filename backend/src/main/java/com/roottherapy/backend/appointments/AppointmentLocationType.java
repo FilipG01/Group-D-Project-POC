eb8e3997fbb040159254834f3abd7c66f21fd8a1
SeanByr <seanbyrne123@outlook.ie>
2026-07-27 21:25:12 +0100

    added appointment enum types

---
+++ backend/src/main/java/com/roottherapy/backend/appointments/AppointmentLocationType.java
@@ -0,0 +1,7 @@
+package com.roottherapy.backend.appointments;
+
+public enum AppointmentLocationType {
+    ONLINE,
+    IN_PERSON,
+    PHONE
+}
---
+++ backend/src/main/java/com/roottherapy/backend/appointments/AppointmentStatus.java
@@ -0,0 +1,9 @@
+package com.roottherapy.backend.appointments;
+
+public enum AppointmentStatus {
+    REQUESTED,
+    CONFIRMED,
+    COMPLETED,
+    CANCELLED,
+    NO_SHOW
+}
